6ecbbb94aa984400ad994cbf8bfe14561d9fa846
Xiaoming Yang <125225518+profxyang@users.noreply.github.com>
2023-12-03 20:52:47 -0500

    Simplified edge finding method

--- r/fun_get_routes.R
+++ r/fun_get_routes.R
@@ -1,68 +1,66 @@
 #function file of get_routes()
 #get unique routes from an sfc object of road geometry
 #the road geometry sfc can be extracted from OpenStreetMap
-#the output of the function is an sfc, a collection of unique routes
+#the output of the function is an sfc with shapes of unique routes
 #call this function for motorway and motorway links separately. 
 #this function requires the stnetworks package.
 library(sfnetworks)
+library(tidygraph)
+library(igraph)
 
-get_routes <- function(sfc){ #sfc<-mtlk_line
-  net = as_sfnetwork(sfc,directed=T)
+get_routes <- function(sfc){ # sfc<-mtlk_line[,'lanes']
+  
+  
+  # Step 1 convert the routes sfc to a sf network object
+  sfc<-sfc[,'lanes'] # keep the data simple
+  net <- as_sfnetwork(sfc,directed=T) 
+  
+  edge_colors = function(x) rep(sf.colors(12, categorical = TRUE)[-2], 2)[c(1:ecount(x))]
+  plot(st_geometry(net, "edges"), col = edge_colors(net), lwd = 2)
+  plot(st_geometry(net, "nodes"), pch = 20, cex = 1, add = TRUE)
+  title(main='Original Network')
+  
+  #Step 2 processing the network
+  #2.1 simplify - remove loop edges and multiple edges
+  simp_net <- net %>%
+    activate("edges") %>%
+    filter(!edge_is_multiple()) %>%
+    filter(!edge_is_loop())
+  plot(st_geometry(sm_simp_net, "edges"), col = edge_colors(net), lwd = 2,title='simple')
+  plot(st_geometry(sm_simp_net, "nodes"), pch = 20, cex = 1, add = TRUE)
+  title(main='Simplified Network')
+  #2.2 smooth - remove psudeo nodes (middle nodes)
+  sm_simp_net<-convert(simp_net,to_spatial_smooth)
+  plot(st_geometry(sm_simp_net, "edges"), col = edge_colors(net), lwd = 2)
+  plot(st_geometry(sm_simp_net, "nodes"), pch = 20, cex = 1, add = TRUE)
+  title(main='Smoothed Simplified Network')
   
   # find all the starting points (never showed up in the "to" column in the net)
-  sfc_edges<-st_as_sf(net, "edges")
-  start_pts<-setdiff(sfc_edges$from,sfc_edges$to)
+  # and all the end points (never showed up in the "from" column in the net)
+  sfc_edges<-st_as_sf(sm_simp_net, "edges")
+  start_pts<-setdiff(sfc_edges$from,sfc_edges$to) 
+  end_pts<-setdiff(sfc_edges$to,sfc_edges$from) 
   
-  paths = st_network_paths(net, from = start_pts[1], type = "all_simple" )
-  
+  # find all simple paths form all "start_pts" to all "end_pts"
+  # for some reason this only returns node paths
+  n_paths = st_network_paths(sm_simp_net, from = start_pts[1], to=end_pts,type = "all_simple" )
   for (i in start_pts[-1]){
-    pathi = st_network_paths(net, from = i, type = "all_simple" )
-    paths = rbind(paths,pathi)
+    pathi = st_network_paths(sm_simp_net, from = i, to=end_pts, type = "all_simple" )
+    n_paths = rbind(n_paths,pathi)
   }
   
-  #remove paths that are part of another path, resulting in a list of unique paths
-  pick<-rep(1,nrow(paths))
   
-  for (i in 1:nrow(paths)){
-    a<-paths[i,]%>%pull(node_paths)%>%unlist() #the node path to be examined
-    blist<-paths[-i,] #the node paths to be compared with a
-    for (j in 1:nrow(blist)){
-      b<-blist[j,]%>%pull(node_paths)%>%unlist()
-      if (all(a %in% b)){pick[i]=0}
-    }
+  #build the edge path from the node path (uniq_paths).
+  find_epath<-function(array){
+    ep<-st_network_paths(sm_simp_net, from=head(array,1),to=tail(array,1)) %>% pull(edge_paths) %>% unlist()
+    return(sfc_edges[ep,] %>% st_union() %>% st_sf())
   }
   
-  uniq_paths<-paths[pick==1,]
+  ls<-lapply(n_paths %>% pull(node_paths),find_epath)
   
-  #build the edge path from the node path (uniq_paths). union edges.
+  e_paths<-do.call(rbind,ls)
   
-  connect_path<-function(arry){
-    df_npath<- data.frame(from=arry,to=lead(arry))%>%drop_na()
-    ls<-apply(df_npath,1,function(x) sfc_edges%>%filter(from==x[1] & to==x[2]))
-    return(do.call(rbind,ls)%>%st_union()) 
-    #This union function sometimes returns multilinestring instead of a linestring
-    #The reason is unknown.
-    #perhaps is because some edge only has two nodes?
-    
-  }
+  cat(nrow(e_paths), " unique paths identified")
   
-  ls_uniq_path<-uniq_paths%>%pull(node_paths) #get the node path from the unique edge path
-  
-  ls_route<-lapply(ls_uniq_path,connect_path)
-  
-  routes <- st_sf(id = 1:nrow(uniq_paths), geometry = do.call(rbind,ls_route), crs = 4326)
-  
-  cat(nrow(routes),"routes identified.")
-  
-  #merge multilinestring routes to linestring
-  clean_geom<-routes%>%st_geometry()
-  for (i in 1:nrow(routes)){ 
-    #this loop removes duplicate coordinates *next to each other* and convert multilinestring to linestring
-    a<-routes[i,]%>%st_coordinates()
-    clean_geom[i]<-a[diff(a[,1])!=0 & diff(a[,2])!=0,1:2]%>%st_linestring(dim = "XY")
-  }
-  #replace the geometry with all linestrings
-  routes<-st_set_geometry(routes,clean_geom)
-  
-  return(routes)
+  return(e_paths)
 }
